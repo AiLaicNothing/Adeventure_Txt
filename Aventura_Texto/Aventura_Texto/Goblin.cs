1046b26a163908097ff41eb466646860ca96eb26
AiLaicNothing <ailaicnothing@gmail.com>
2026-04-30 17:44:18 -0500

    Entidades

---
+++ Aventura_Texto/Aventura_Texto/Goblin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aventura_Texto
+{
+    internal class Goblin: Entity
+    {
+        public Goblin()
+        {
+            name = "Goblin";
+            health = 10;
+            damage = 3;
+        }
+    }
+}
---
+++ Aventura_Texto/Aventura_Texto/Troll.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aventura_Texto
+{
+    internal class Troll: Entity
+    {
+        public Troll()
+        {
+            name = "Troll";
+            damage = 6;
+            health = 20;
+        }
+    }
+}
